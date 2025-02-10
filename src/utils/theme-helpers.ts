c724877cf95af3fbf6656f407c7a52f8d032901c
Salvatore Strangio <sal.strangio@gmail.com>
2025-02-10 20:36:47 +0100

    Cleaning and setting up for first release

--- src/utils/theme-helpers.ts
+++ src/utils/theme-helpers.ts
@@ -1,15 +1,25 @@
+export const setPreferedColorScheme = () => {
+    const html = document.getElementsByTagName("html")[0];
+    if (
+        window.matchMedia("(prefers-color-scheme: dark)").matches &&
+        !html.classList.contains("dark")
+    ) {
+        document.documentElement.classList.add("dark");
+    }
+};
+
 export const handleThemeChange = () => {
-	const html = document.getElementsByTagName('html')[0];
-	const line = document.getElementById('line');
-	line?.classList.add('hidden');
-	html.style.animation = 'shake .25s ease-in-out';
-	setTimeout(() => {
-		html.classList.toggle('dark');
-		cleanAnimation(html, line);
-	}, 250);
+    const html = document.getElementsByTagName("html")[0];
+    const line = document.getElementById("line");
+    line?.classList.add("hidden");
+    html.style.animation = "shake .25s ease-in-out";
+    setTimeout(() => {
+        html.classList.toggle("dark");
+        cleanAnimation(html, line);
+    }, 250);
 };
 
 const cleanAnimation = (html: HTMLHtmlElement, line: HTMLElement | null) => {
-	html.style.animation = '';
-	line?.classList.remove('hidden');
+    html.style.animation = "";
+    line?.classList.remove("hidden");
 };

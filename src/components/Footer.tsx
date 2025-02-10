dedf87395a991c5c2adbfab01d3a77989b7a5267
Salvatore Strangio <sal.strangio@gmail.com>
2025-02-10 12:12:16 +0100

    Footer i18n, Logo tmp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { MapPinLine } from "@phosphor-icons/react";
+import { useTranslation } from "react-i18next";
 
 export const Footer = () => {
+    const { t } = useTranslation();
     return (
         <footer className="absolute bottom-0.5 left-1/2 flex -translate-x-1/2 items-center gap-1 text-sm whitespace-nowrap sm:bottom-1 sm:text-base">
             © 2025 Salvatore Strangio -
@@ -10,7 +12,7 @@ export const Footer = () => {
                 className="flex gap-1 underline-offset-3 transition-all hover:text-accent-light hover:underline dark:hover:text-accent-dark"
             >
                 <MapPinLine size={18} className="translate-y-0.75" />
-                Venice, Italy
+                {t("ve_it")}
             </a>
         </footer>
     );
--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,19 +1,21 @@
-// import wave from "../assets/images/wave.png";
+import wave from "../assets/images/wave.png";
 import { LangSelect, ThemeToggle } from "./ui";
 
 export const HeaderNav = () => {
     return (
-        <nav className="absolute top-2 right-1/2 z-10 grid w-fit min-w-xs translate-x-1/2 animate-fade-in-slide-down grid-cols-[1fr_auto_1fr_auto_auto] gap-2 rounded-xl border border-accent-light/50 bg-primary-light/15 p-4 shadow-light backdrop-blur-xs sm:top-9 dark:border-accent-dark/30 dark:bg-primary-dark/10 dark:shadow-dark">
+        <nav className="absolute top-2 right-1/2 z-10 grid w-fit min-w-xs translate-x-1/2 animate-fade-in-slide-down grid-cols-[1fr_auto_1fr_auto_auto] gap-2 rounded-xl border border-accent-light/50 bg-primary-light/15 px-4 py-2 shadow-light backdrop-blur-xs sm:top-9 dark:border-accent-dark/30 dark:bg-primary-dark/10 dark:shadow-dark">
             <div className="my-auto h-0.25 max-w-xs min-w-0 bg-accent-light dark:bg-accent-dark" />
-            <h1 className="!font-questrial leading-none text-accent-light drop-shadow-light dark:text-accent-dark dark:drop-shadow-dark">
-                Sal
-            </h1>
-            {/* <img
+            <div className="relative mx-2">
+                <img
                     src={wave}
                     alt="Origami Kanagawa wave logo"
-                    width={70}
+                    width={80}
                     className="sticker"
-                /> */}
+                />
+                <h3 className="sticker-themed absolute top-1/2 left-1/2 -translate-1/2 !font-questrial text-2xl leading-none whitespace-nowrap text-accent-light dark:text-accent-dark dark:drop-shadow-dark">
+                    {"< "}S{" />"}
+                </h3>
+            </div>
 
             <div className="my-auto h-0.25 max-w-xs min-w-0 bg-accent-light dark:bg-accent-dark" />
 

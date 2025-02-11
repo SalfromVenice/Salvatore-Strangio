1b01b81bf29977349ad819f830fe723961ac3020
Salvatore Strangio <sal.strangio@gmail.com>
2025-02-11 19:43:36 +0100

    FIX: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    base: process.env.VITE_BASE_URL,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        base: env.VITE_BASE_URL,
+        plugins: [react(), tailwindcss()],
+    };
 });
